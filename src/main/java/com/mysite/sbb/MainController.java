e1db83936b12773d1a4c27a43725374f90f22f95
ahcoo <mynameisjins@gmail.com>
2022-09-19 17:52:51 +0900

    220919 lecture

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,14 +2,20 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class MainController {
 
     @RequestMapping("/sbb")
-    public void index() {
+    @ResponseBody //반환할 값을 문자열로 만들어서 보내겠다는 뜻.
+
+    public String index() {
+
         System.out.println("sbb");
+
+        return "sbb";
 
     }
 }
