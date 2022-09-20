50024e143e12affaa1606f436f3205d0c5605144
ahcoo <mynameisjins@gmail.com>
2022-09-20 17:16:12 +0900

    0920-GetMapping/PostMapping/RequestParam

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -3,6 +3,9 @@ package com.mysite.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 @Controller
 public class MainController {
@@ -17,7 +20,7 @@ public class MainController {
         return "sbb";
 
     }
-
+// 0920 수업시작
     @GetMapping("/page1")
     @ResponseBody
     public String showPage1() {
@@ -40,7 +43,7 @@ public class MainController {
              """.formatted(age);
     }
 
-    @PostMapping("/page2")
+    @GetMapping("/page2")
     @ResponseBody
     public String showPage2Get(@RequestParam(value="age", defaultValue="0") int age) {
 
@@ -48,6 +51,77 @@ public class MainController {
              <h1>입력된 나이 : %d</h1>
              <h1>POST방식으로 옴</h1>
              """.formatted(age);
+
     }
 
+//    @GetMapping("/plus")
+//    @ResponseBody
+//    public int plus(@RequestParam int a, @RequestParam int b) {
+//        int c = a + b;
+//        return c;
+//    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public String plus(@RequestParam(required = false) Integer a, @RequestParam(required = false) Integer b) {
+        if ( a == null ) {
+            return "a를 입력해주세요.";
+        }
+        if ( b == null ) {
+            return "b를 입력해주세요.";
+        }
+        return String.valueOf(a+b);
+    }
+
+//    @GetMapping("/minus")
+//    @ResponseBody
+//    public int minus(@RequestParam int a, @RequestParam int b) {
+//        int c = a - b;
+//        return c;
+//    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public String minus(@RequestParam(required = false) Integer a, @RequestParam(required = false) Integer b) {
+        if ( a == null ) {
+            return "a를 입력해주세요.";
+        }
+        if ( b == null ) {
+            return "b를 입력해주세요.";
+        }
+        return String.valueOf(a-b);
+    }
+//    int num;
+//    @GetMapping("/increase")
+//    @ResponseBody
+//    public int increase() {
+//                num++;
+//                return num;
+//    }
+
+    int increaseNum;
+    @GetMapping("/increase")
+    @ResponseBody
+    public int increase() {
+
+        return increaseNum++;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String gugudan(int dan, int limit) {
+//        String result = "";
+//        for ( int i = 1; i <= limit; i++ ) {
+//            result += dan + " * " + i + " = " + (dan * i) + "<br/>\n";
+//            }
+//        return result;
+//
+//    }
+        final Integer finalDan = dan;
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
+                .collect(Collectors.joining("<br>\n"));
+    }
+    // 0920 수업끝
 }
+
